2936c999dfbfdd2ac4b45afec420a57c3964b957
mao nguyen <lgold141@gmail.com>
2024-05-08 06:16:18 +0700

    fix: load import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { TransformInterceptor } from './app/interceptor/transform.interceptor';
-import { NODE_ENV, PORT, SWAGGER, WHITE_LIST_IP } from './config';
+import { PORT, SWAGGER } from './config';
 import { setupSwagger } from './swagger';
 
 async function bootstrap() {
@@ -19,7 +19,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
-  console.log('hack', WHITE_LIST_IP);
   if (SWAGGER) {
     setupSwagger(app);
   }
