535a64e14f93aa3982242bc31074ff55d192fd08
warjamma <lgold141@gmail.com>
2025-03-07 11:31:59 +0700

    fix: hidden module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,23 +4,23 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import {
-  AuthModule,
-  LanguagesModule,
-  AdminSectionsModule,
-  SectionsModule,
-  UserModule,
+  // AuthModule,
+  // LanguagesModule,
+  // AdminSectionsModule,
+  // SectionsModule,
+  // UserModule,
   CrawlModule,
   KSplatModule,
 } from './app';
- 
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    AuthModule,
-    UserModule,
-    LanguagesModule,
-    AdminSectionsModule,
-    SectionsModule,
+    // AuthModule,
+    // UserModule,
+    // LanguagesModule,
+    // AdminSectionsModule,
+    // SectionsModule,
     CrawlModule,
     KSplatModule,
     // UploadModule,
