0e47d2acbe08843f7876e2c9c89323a52f32a9bf
warjamma <lgold141@gmail.com>
2025-03-10 23:51:07 +0700

    fix: update headless

--- src/app/crawl-view-360/service/crawl-snap-shot-360-google-earth/crawl-snap-shot-360-google-earth.service.ts
+++ src/app/crawl-view-360/service/crawl-snap-shot-360-google-earth/crawl-snap-shot-360-google-earth.service.ts
@@ -13,7 +13,7 @@ export const captureGoogleEarth = async (
   location: string,
 ): Promise<ICaptureGoogleEarth> => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: {
       width: 1920,
       height: 1080,
