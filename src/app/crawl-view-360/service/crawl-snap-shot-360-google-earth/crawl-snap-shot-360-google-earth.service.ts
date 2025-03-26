a6998952b4cf6a6346997de642404e2a44a77d33
warjamma <lgold141@gmail.com>
2025-03-26 23:16:49 +0700

    fix: run debuglogs

--- src/app/crawl-view-360/service/crawl-snap-shot-360-google-earth/crawl-snap-shot-360-google-earth.service.ts
+++ src/app/crawl-view-360/service/crawl-snap-shot-360-google-earth/crawl-snap-shot-360-google-earth.service.ts
@@ -58,6 +58,8 @@ export const captureGoogleEarth = async (
 
   const page = await browser.newPage();
 
+  await page.setDefaultTimeout(60000);
+
   await page.setUserAgent(
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
   );
